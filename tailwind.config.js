e0a8233fb7a3f2c1487e1ab5a1f2bb1e532fff80
Venish Patel <venish.g.patel@gmail.com>
2023-04-22 06:07:29 -0700

    custom breakpoint added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      'tablet': '640px',
+      'laptop': '992px',
+      'desktop': '1280px'
     },
   },
   plugins: [],
