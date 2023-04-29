6615e3a96eab35c20d4342f6ebaab281aa2017fc
Venish Patel <venish.g.patel@gmail.com>
2023-04-29 06:20:52 -0700

    Added color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,33 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
     './app/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: ['class', '[data-mode="dark"]'],
   theme: {
     screens: {
       'tablet': '640px',
       'laptop': '992px',
       'desktop': '1280px'
     },
+    colors: {
+      primary: {
+        DEFAULT: 'var(--color-primary)',
+        variant: '',
+        disabled: ''
+      },
+      secondary: {
+        DEFAULT: 'var(--color-secondary)',
+        variant: '',
+        disabled: ''
+      },
+      surface: 'var(--color-surface)',
+      interactive: 'var(--color-interactive)',
+      error: 'var(--color-error)',
+      black: "#000000",
+      white: "#ffffff,"
+    },
+    fontFamily: {
+      'quicksand': ['QuickSand', 'sans-serif']
+    }
   },
   plugins: [],
 }
