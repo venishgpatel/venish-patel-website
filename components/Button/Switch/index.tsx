946693bef4b13cc01bab30087aea1170a6304a71
Venish Patel <venish.g.patel@gmail.com>
2023-05-17 17:17:00 -0700

    Color Theme Switcher functionality updated.

--- components/Button/Switch/index.tsx
+++ components/Button/Switch/index.tsx
@@ -1,28 +1,27 @@
 "use client"
 import { Switch } from "@headlessui/react"
 import { useTheme } from "next-themes"
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 const SwitchColorTheme = () => {
   const [mounted, setMounted] = useState(false)
   const { resolvedTheme, setTheme } = useTheme()
-  const isDarkTheme = resolvedTheme === 'dark'
+
+  const isDarkTheme = useMemo(() => {
+    return resolvedTheme === 'dark'
+  }, [resolvedTheme, mounted])
 
   useEffect(() => {
     setMounted(true)
   }, [])
 
-  if (!mounted) {
-    return null
-  }
-  
   return (
     <Switch
       checked={isDarkTheme}
       onChange={() => resolvedTheme === 'dark'? setTheme('light'): setTheme('dark')}
       className='bg-primary w-14 h-7 inline-flex items-center rounded-full'
     >
-      <span className={`cmp-switch__slider ${isDarkTheme ? 'translate-x-8' : 'translate-x-1'} inline-block w-5 h-5 bg-surface rounded-full transition duration-500`}></span>
+      <span className={`cmp-switch__slider ${!mounted ? 'translate-x-[18px]' : isDarkTheme ? 'translate-x-8' : 'translate-x-1'} inline-block w-5 h-5 bg-surface rounded-full transition duration-500`}></span>
     </Switch>
   )
 }
